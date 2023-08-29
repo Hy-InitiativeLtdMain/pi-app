b5006acfb8b5b1c9ba866fb8d09d4ff00653747c
dephizee <hafiz227@gmail.com>
2023-08-29 11:03:52 +0100

    feat: payout

--- app/Services/Payment/PaystackService.php
+++ app/Services/Payment/PaystackService.php
@@ -132,7 +132,7 @@ class PaystackService
             'reference' => $ref
         ]);
         $_resp = $resp->json();
-        dd($_resp);
+        dd($_resp['status']);
         $transfer_code = $_resp['data']['transfer_code'];
         $transaction->transfer_code = $transfer_code;
         $transaction->save();
