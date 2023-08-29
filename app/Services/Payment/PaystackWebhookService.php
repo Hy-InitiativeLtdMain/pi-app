693605894281560a27767762321fa2d81c3558d8
dephizee <hafiz227@gmail.com>
2023-08-29 05:39:56 +0100

    feat: webhook

--- app/Services/Payment/PaystackWebhookService.php
+++ app/Services/Payment/PaystackWebhookService.php
@@ -7,14 +7,18 @@ use App\Models\Transaction;
 use App\Services\Admin\UserService;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
 class PaystackWebhookService
 {
-    public function chargeSuccess($_data )
+    public function chargeSuccess($_data)
     {
+        Log::info('_data == ' . print_r($_data, true));
         $transaction = Transaction::whereRef($_data['reference'])->firstOrFail();
-        if(abs(floatval($transaction->total_amount))*100 == floatval($_data['amount']) ){
+        Log::info('transaction == ' . print_r($transaction, true));
+
+        if (abs(floatval($transaction->total_amount)) * 100 == floatval($_data['amount'])) {
             $transaction->status = 1;
             $transaction->paid_at = Carbon::now();
             $transaction->save();
@@ -25,18 +29,17 @@ class PaystackWebhookService
             //     $_transaction->save();
             // }
             $data['message'] = 'Updated';
+            Log::info('data == ' . print_r($data, true));
             return response()->json($data, 200);
         }
-
         $data['message'] = 'Not found';
         return response()->json($data, 404);
-
     }
 
-    public function transferSuccess($_data )
+    public function transferSuccess($_data)
     {
         $transaction = Transaction::whereRef($_data['reference'])->firstOrFail();
-        if(abs(floatval($transaction->amount))*100 == floatval($_data['amount']) ){
+        if (abs(floatval($transaction->amount)) * 100 == floatval($_data['amount'])) {
             $transaction->status = 1;
             $transaction->paid_at = Carbon::now();
             $transaction->save();
@@ -48,8 +51,5 @@ class PaystackWebhookService
         }
         $data['message'] = 'Not found';
         return response()->json($data, 404);
-
     }
-
-
 }
