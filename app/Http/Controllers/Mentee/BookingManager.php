56b445c24a2a48c90d25343a5b1185c6891b34f3
daking24 <watcher24.inc@gmail.com>
2024-03-13 11:14:21 +0100

    Update mentee booking manager controller and available mentors resource to include booking status

--- app/Http/Controllers/Mentee/BookingManager.php
+++ app/Http/Controllers/Mentee/BookingManager.php
@@ -62,16 +62,33 @@ class BookingManager extends Controller
         // Retrieve the date from the request
         $date = $request->input('date');
 
+        // Error handling
+        if (!auth()->user()->mentee) {
+            return $this->errorResponse('You are not a mentee', 401);
+        }
+        // get signed-in mentee id
+        $menteeId = auth()->user()->mentee->id;
+
+
+        //retrieve the signed-in mentee's bookings
+        $menteeBookings = Booking::where('mentee_id', $menteeId)->pluck('mentor_availability_id');
+
         // Paginate the MentorAvailability records
         $perPage = $request->input('per_page', 10); // Default to 10 items per page
         $allAvailability = MentorAvailability::all();
 
         // Filter the records based on the date field within the availability JSON
-        $availableMentors = $allAvailability->filter(function ($availability) use ($date) {
+        $availableMentors = $allAvailability->filter(function ($availability) use ($date, $menteeBookings) {
             // Decode the availability field only if it's a string
             $availabilityData = is_string($availability->availability) ? json_decode($availability->availability, true) : $availability->availability;
             // Check if the decoded data has the expected structure and date
-            return is_array($availabilityData) && isset ($availabilityData['date']) && $availabilityData['date'] === $date;
+            $isAvailable = is_array($availabilityData) && isset ($availabilityData['date']) && $availabilityData['date'] === $date;
+            // Check if the mentor has been booked
+            $isBooked = $menteeBookings->contains($availability->id);
+            // Add a temporary field to the availability record to indicate if the mentor is booked
+            $availability->is_booked = $isBooked ? 'yes':'no';
+            // Return the availability record with the temporary field
+            return $isAvailable;
         });
 
         // Return the paginated response
