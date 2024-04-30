8e82b0385081daac524094ba1c3b3ea88846a51f
daking24 <watcher24.inc@gmail.com>
2024-04-30 15:51:24 +0100

    Stuff

---
+++ database/migrations/2024_04_30_124702_create_mentor_experiences_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mentor_experiences', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('mentor_id')->constrained('mentors')->onDelete('cascade');
+            $table->string('employment_type')->nullable();
+            $table->string('company_name')->nullable();
+            $table->string('job_title')->nullable();
+            $table->string('location')->nullable();
+            $table->string('location_type')->nullable();
+            $table->boolean('current_job')->default(false);
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mentor_experiences');
+    }
+};
