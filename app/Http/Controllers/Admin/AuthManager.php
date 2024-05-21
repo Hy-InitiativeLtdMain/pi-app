038f15454787d23aede63de486e63abc4c99af2b
daking24 <watcher24.inc@gmail.com>
2024-05-21 14:25:58 +0100

    feat: Mass User Role Update

--- app/Http/Controllers/Admin/AuthManager.php
+++ app/Http/Controllers/Admin/AuthManager.php
@@ -24,5 +24,32 @@ class AuthManager extends Controller
         return response($registerData['data'], $registerData['code']);
     }
 
-    
+    public function updateUserRole(Request $request, User $user)
+    {
+        $validated = $request->validate([
+            'is_admin' => 'required|boolean'
+        ]);
+
+        $user->update($validated);
+
+        return response()->json("User Role has been Updated", 200);
+    }
+
+    public function updateUsersRoles(Request $request)
+    {
+        $request->validate([
+            'ids' => 'required|array',
+            'ids.*' => 'exists:users,id',
+            'is_admin' => 'required|boolean'
+        ]);
+
+        // Retrieve the list of IDs from the request
+        $ids = $request->input('ids');
+
+        $users = User::whereIn('id', $ids)->update([
+            'is_admin' => $request->input('is_admin')
+        ]);
+
+        return response()->json('Users Roles have been updated.', 200);
+    }
 }
