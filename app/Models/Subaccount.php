4f2eb86e909472380a295b5f6598f9f67782a233
daking24 <watcher24.inc@gmail.com>
2025-01-15 14:48:30 +0100

    Add fillable properties and relationships to Subaccount model: define user and bankAccount relationships for improved data handling.

--- app/Models/Subaccount.php
+++ app/Models/Subaccount.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Subaccount extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'subaccount_code',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function bankAccount()
+    {
+        return $this->hasOne(BankAccount::class);
+    }
 }
