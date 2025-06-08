449be28172f445bfd337341db853c5b0db60eb5e
daking24 <watcher24.inc@gmail.com>
2025-06-08 17:29:11 +0100

    Make mentor columns nullable in database Allow optional mentor information by setting nullable columns in mentor table

---
+++ database/migrations/2025_06_08_161439_make_mentor_columns_nullable.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('mentors', function (Blueprint $table) {
+            $table->string('firstname')->nullable()->change();
+            $table->string('lastname')->nullable()->change();
+            $table->string('email')->nullable()->change();
+            $table->string('phone')->nullable()->change();
+            $table->string('company')->nullable()->change();
+            $table->string('job_title')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('mentors', function (Blueprint $table) {
+            $table->string('firstname')->nullable(false)->change();
+            $table->string('lastname')->nullable(false)->change();
+            $table->string('email')->nullable(false)->change();
+            $table->string('phone')->nullable(false)->change();
+            $table->string('company')->nullable(false)->change();
+            $table->string('job_title')->nullable(false)->change();
+        });
+    }
+};
